bb8c99dd187471c863a4159d066d97dc81c622df
Nguyễn Sĩ Huấn <nguyensihuan711994@gmail.com>
2026-09-23 17:03:06 +0700

    refactor: expose release manifest API

--- planning/publish/__init__.py
+++ planning/publish/__init__.py
@@ -19,6 +19,14 @@ from .proposal import (
     save_proposal_artifacts,
     with_proposal_identity,
 )
+from .release import (
+    RELEASE_MANIFEST_FILE,
+    RELEASE_SCHEMA,
+    RELEASE_SCHEMA_VERSION,
+    build_release_manifest,
+    save_release_manifest,
+    write_release_manifest,
+)
 from .runner import main, parse_args
 from .service import run_pipeline_with_retry
 from .snapshot import read_snapshot
@@ -45,10 +53,14 @@ __all__ = [
     "PROPOSAL_WORKBOOK_FILE",
     "PUBLISH_DECISION_FILE",
     "READY_PUBLISH_STATUSES",
+    "RELEASE_MANIFEST_FILE",
+    "RELEASE_SCHEMA",
+    "RELEASE_SCHEMA_VERSION",
     "REVIEW_REQUIRED_STATUS",
     "SOURCES",
     "VOLATILE_XLSX_PARTS",
     "blocked_decision",
+    "build_release_manifest",
     "detect_input_changes",
     "main",
     "parse_args",
@@ -58,7 +70,9 @@ __all__ = [
     "read_snapshot",
     "run_pipeline_with_retry",
     "save_proposal_artifacts",
+    "save_release_manifest",
     "save_publish_decision",
     "save_states_after_success",
     "with_proposal_identity",
+    "write_release_manifest",
 ]
